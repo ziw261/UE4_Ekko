3548c1554e629c1419cbaef60ff0e2920793a20d
Azusa <akiriin0@gmail.com>
2021-01-11 12:05:33 -0500

    Update EkkoCharacter.h

--- Source/Ekko/EkkoCharacter.h
+++ Source/Ekko/EkkoCharacter.h
@@ -68,5 +68,11 @@ public:
 	FORCEINLINE class USpringArmComponent* GetCameraBoom() const { return CameraBoom; }
 	/** Returns FollowCamera subobject **/
 	FORCEINLINE class UCameraComponent* GetFollowCamera() const { return FollowCamera; }
+
+
+private:
+
+	float Health = 100.f;
+	
 };
 
